dc3f6456ac26340f41520da0209bced27555f9d3
jhk1090 <jhk1090.public@gmail.com>
2023-10-27 17:06:10 +0900

    temporary hold

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+[[ linkto | {{{#!folding
+
+}}}]]
+```
+
+unlink
--- src/elem.ts
+++ src/elem.ts
@@ -56,7 +56,6 @@ export class MacroElem extends UnableChild {
     }
     override flushArr(array: Elem[]): Elem[] {
         const flag = {
-            isBroken: false,
             isError: false,
             isSkippable: false
         };
@@ -164,7 +163,6 @@ export class LinkElem extends AbleChild {
     }
     override flushArr(array: Elem[]): Elem[] {
         const flag = {
-            isBroken: false,
             isError: false,
             isSkippable: false
         }
@@ -196,7 +194,7 @@ export class LinkElem extends AbleChild {
                     case "CONTAIN": {
                         const { status: substatus, common: subcommon } = elem.availableRange.compare(common as Range);
                         if (substatus === "CONTAIN" || substatus === "SAME") {
-                            this.setParent(elem.uuid);;
+                            this.setParent(elem.uuid);
                         } else {
                             flag.isError = true;
                         }
@@ -221,8 +219,9 @@ export class LinkElem extends AbleChild {
                         const {status: substatus, common: subcommon} = this.availableRange.compare(common as Range);
                         if (substatus === "CONTAIN" || substatus === "SAME") {
                             elem.setParent(this.uuid)
+                        } else {
+                            flag.isSkippable = true;
                         }
-                        flag.isSkippable = true;
                         break;
 
                     // [[ [macro( ]] )]
@@ -233,6 +232,7 @@ export class LinkElem extends AbleChild {
                         } else {
                             flag.isError = true;
                         }
+                        break
                     default:
                         break;
                 }
@@ -258,7 +258,6 @@ export class ULinkElem extends UnableChild {
 
     override flushArr(array: Elem[]): Elem[] {
         const flag = {
-            isBroken: false,
             isError: false,
             isSkippable: false
         }
@@ -290,7 +289,7 @@ export class ULinkElem extends UnableChild {
                     case "CONTAIN": {
                         const { status: substatus, common: subcommon } = elem.availableRange.compare(common as Range);
                         if (substatus === "CONTAIN" || substatus === "SAME") {
-                            this.setParent(elem.uuid);;
+                            this.setParent(elem.uuid);
                         } else {
                             flag.isError = true;
                         }
@@ -321,6 +320,7 @@ export class ULinkElem extends UnableChild {
                         } else {
                             flag.isError = true;
                         }
+                        break;
                     default:
                         break;
                 }
@@ -332,6 +332,248 @@ export class ULinkElem extends UnableChild {
         }
 
         if (!flag.isError) temp.push(this);
+        array = temp;
+        return array;
+    }
+}
+
+export class BracketFoldingElem extends AbleChild {
+    summary: string = ""
+    detail: string = ""
+    availableRange: Range = new Range(0, 1);
+    constructor(summary: string, detail: string, range: Range, availableRange: Range) {
+        super(range);
+        this.summary = summary;
+        this.detail = detail;
+        this.availableRange = availableRange;
+    }
+
+    override flushArr(array: Elem[]): Elem[] {
+        const flag = {
+            isThisSkipped: false,
+            isElemSkipped: false
+        }
+        const temp = [];
+        for (const [idx, elem] of array.entries()) {
+            flag.isElemSkipped = false;
+            const {status, common} = elem.range.compare(this.range);
+
+            if (elem.range.start > this.range.end) {
+                temp.push(...array.slice(idx));
+                break;
+            }
+
+            if (elem instanceof CommentElem) {
+                switch (status) {
+                    /*
+                    특이하게 CommentElem이 우선순위가 더 높음
+                    ## {{{#!folding sdf
+                    }}} 
+                    {{{#!folding asdf
+                    ## comment }}}*/
+                    case "OVERLAP":
+                        flag.isThisSkipped = true;
+                        break;
+                    /*
+                    {{{#!folding asdf
+                    ## comment
+                    }}}
+                    */
+                    case "REVERSE_CONTAIN": {
+                        const { status: substatus, common: subcommon} = this.availableRange.compare(common as Range);
+                        if (substatus === "CONTAIN" || substatus === "SAME") {
+                            elem.setParent(this.uuid)
+                        } else {
+                            flag.isElemSkipped = true;
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+
+            if (elem instanceof HeadingElem) {
+                switch (status) {
+                    /*
+                    == {{#!folding ==
+                    }}}
+                    {{#!folding
+                    == }}} ==
+                    */
+                   case "OVERLAP":
+                        flag.isThisSkipped = true;
+                        break;
+                    /*
+                    {{{#!folding
+                    == asdf ==
+                    }}}
+                    */
+                    case "REVERSE_CONTAIN":
+                        flag.isThisSkipped = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (elem instanceof MacroElem) {
+                switch (status) {
+                    /* 
+                    [macro(
+                    {{{#!folding 
+                    }}}
+                    )]
+                    */
+                    case "CONTAIN":
+                        flag.isThisSkipped = true;
+                        break;
+                    /* 
+                    {{{#!folding asdf
+                    [macro( )]
+                    }}}
+                    */
+                    case "REVERSE_CONTAIN": {
+                        const { status: substatus, common: subcommon} = this.availableRange.compare(common as Range);
+                        if (substatus === "CONTAIN" || substatus === "SAME") {
+                            elem.setParent(this.uuid)
+                        } else {
+                            flag.isElemSkipped = true;
+                        }
+                        break;
+                    }
+                    /* 
+                    {{{#!folding asdf
+                    [dday(
+                    }}}
+                    )]
+                    [dday(
+                    {{{#!folding asdf
+                    )]
+                    }}}
+                    */
+                    case "OVERLAP":
+                        if (this.range.start < elem.range.start) {
+                            flag.isElemSkipped = true;
+                        } else {
+                            flag.isThisSkipped = true;
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+
+            if (elem instanceof LinkElem) {
+                switch (status) {
+                    /* 
+                    {{{#!folding asdf
+                    [[asdf]]
+                    }}}
+                    */
+                   case "REVERSE_CONTAIN": {
+                        const { status: substatus, common: subcommon} = this.availableRange.compare(common as Range);
+                        if (substatus === "CONTAIN" || substatus === "SAME") {
+                            elem.setParent(this.uuid)
+                        } else {
+                            flag.isElemSkipped = true;
+                        }
+                        break;
+                    }
+                    /*
+                    먼저연 folding 우선
+                    {{{#!folding asdf
+                    [[ }}} | ]]
+
+                    먼저연 folding 우선
+                    {{{#!folding asdf
+                    [[ | }}} ]]
+
+                    먼저연 link 우선
+                    [[ {{{#!folding asdf | ]]
+                    }}}
+
+                    나중에연 folding 우선
+                    [[ | {{{#!folding asdf ]]
+                    }}}
+                    */
+                    case "OVERLAP": {
+                        if (this.range.start < elem.range.start) {
+                            flag.isElemSkipped = true;
+                        } else {
+                            // this.range.start > elem.range.start
+                            // this.range.start > elem.availableRange.start
+                            if (this.range.start > elem.availableRange.start) {
+                                flag.isElemSkipped = true;
+                            } else {
+                                flag.isThisSkipped = true;
+                            }
+                        }
+                        break;
+                    }
+                    /*
+                    [[asdf|{{{#!folding
+                    d
+                    }}}]]
+                    */
+                    case "CONTAIN": {
+                        const { status: substatus, common: subcommon } = elem.availableRange.compare(common as Range);
+                        if (substatus === "CONTAIN" || substatus === "SAME") {
+                            this.setParent(elem.uuid);
+                        } else {
+                            flag.isThisSkipped = true;
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+
+                }
+            }
+
+            if (elem instanceof ULinkElem) {
+                switch (status) {
+                    /*
+                    {{{#!folding asdf
+                    [[asdf]]
+                    }}}
+                    */
+                    case "REVERSE_CONTAIN": {
+                        const { status: substatus, common: subcommon} = this.availableRange.compare(common as Range);
+                        if (substatus === "CONTAIN" || substatus === "SAME") {
+                            elem.setParent(this.uuid)
+                        } else {
+                            flag.isElemSkipped = true;
+                        }
+                        break;
+                    }
+                    /*
+                    {{{#!folding asdf
+                    [[ }}} ]]
+
+                    [[ {{{#!folding asdf ]]
+                    }}}
+                    */
+                    case "OVERLAP": {
+                        if (this.range.start < elem.range.start) {
+                            flag.isElemSkipped = true;
+                        } else {
+                            flag.isThisSkipped = true;
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+
+            if (!flag.isElemSkipped) {
+                temp.push(elem);
+            }
+        }
+
+        if (!flag.isThisSkipped) temp.push(this);
         array = temp;
         return array;
     }
