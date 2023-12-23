02d876c64bce3596aeb4f1b30c489076d553a202
jhk1090 <jhk1090.public@gmail.com>
2023-12-23 18:10:09 +0900

    temp;complete; tripleBracket is now ignoring children depending on its type, fix performance issue while executing firstGrouping

--- src/index.ts
+++ src/index.ts
@@ -588,21 +588,8 @@ export class NamuMark {
             MathTagClose: [processMathTagClose],
         };
         const firstGrouping = () => {
-            for (let idx = 0; idx < this.holderArray.length; idx++) {
-                const elem = this.holderArray[idx];
-
-                this.holderArray = this.holderArray.filter((v) => {
-                    if (v.ignore) {
-                        v.group.forEach((group) => this.removeGroup({ group }));
-                        return false;
-                    }
-                    return true;
-                });
-
-                if (elem.fixed) {
-                    continue;
-                }
-
+            const flag = { skipFixing: false };
+            const mainGrouping = (elem: HolderElem, idx: number) => {
                 const doubleSquare = elem.group.find((v) => v.type === "DoubleSquareBracket");
                 if (doubleSquare !== undefined) {
                     const foundGroup = elem.group.find((v) => v.type === "SingleSquareBracket");
@@ -611,7 +598,8 @@ export class NamuMark {
                     }
                     if (doubleSquare.elems.length !== 4 && doubleSquare.elems.length !== 5) {
                         this.removeGroup({ group: doubleSquare });
-                        continue;
+                        flag.skipFixing = true;
+                        return;
                     }
                     const start = this.holderArray.findIndex((v) => v.uuid === doubleSquare.elems[1].uuid);
                     const end = this.holderArray.findIndex(
@@ -623,13 +611,15 @@ export class NamuMark {
                         .toSpliced(-1, 1);
                     const pipeIndex = sliced.findIndex((v) => v.type === "Pipe");
                     (pipeIndex !== -1 ? sliced.slice(0, pipeIndex) : sliced).forEach((v) => (v.ignore = true));
+                    return;
                 }
 
                 const singleSquare = elem.group.find((v) => v.type === "SingleSquareBracket");
                 if (singleSquare !== undefined) {
                     if (singleSquare.elems.length !== 2 && singleSquare.elems.length !== 4) {
                         this.removeGroup({ group: singleSquare });
-                        continue;
+                        flag.skipFixing = true;
+                        return;
                     }
                     const start = this.holderArray.findIndex((v) => v.uuid === singleSquare.elems[0].uuid);
                     const end = this.holderArray.findIndex((v) => v.uuid === singleSquare.elems[singleSquare.elems.length - 1].uuid);
@@ -640,29 +630,92 @@ export class NamuMark {
                     sliced.forEach((v) => {
                         if (v.type !== "ParenthesisOpen" && v.type !== "ParenthesisClose") v.ignore = true;
                     });
+                    return;
                 }
 
                 const triple = elem.group.find((v) => v.type === "TripleBracket");
                 if (triple !== undefined) {
                     if (triple.elems.length !== 2) {
                         this.removeGroup({ group: triple });
-                        continue;
+                        flag.skipFixing = true;
+                        return;
                     }
+
+                    const sizingRegex = /^(\+|-)([1-5])( |\n)/g;
+                    const wikiRegex = /^#!wiki([^\}]+)?\n/g;
+                    const htmlRegex = /^#!html/g;
+                    const cssColor =
+                        "black|gray|grey|silver|white|red|maroon|yellow|olive|lime|green|aqua|cyan|teal|blue|navy|magenta|fuchsia|purple|dimgray|dimgrey|darkgray|darkgrey|lightgray|lightgrey|gainsboro|whitesmoke|brown|darkred|firebrick|indianred|lightcoral|rosybrown|snow|mistyrose|salmon|tomato|darksalmon|coral|orangered|lightsalmon|sienna|seashell|chocolate|saddlebrown|sandybrown|peachpuff|peru|linen|bisque|darkorange|burlywood|anaatiquewhite|tan|navajowhite|blanchedalmond|papayawhip|moccasin|orange|wheat|oldlace|floralwhite|darkgoldenrod|goldenrod|cornsilk|gold|khaki|lemonchiffon|palegoldenrod|darkkhaki|beige|ivory|lightgoldenrodyellow|lightyellow|olivedrab|yellowgreen|darkolivegreen|greenyellow|chartreuse|lawngreen|darkgreen|darkseagreen|forestgreen|honeydew|lightgreen|limegreen|palegreen|seagreen|mediumseagreen|springgreen|mintcream|mediumspringgreen|mediumaquamarine|aquamarine|turquoise|lightseagreen|mediumturquoise|azure|darkcyan|darkslategray|darkslategrey|lightcyan|paleturquoise|darkturquoise|cadetblue|powderblue|lightblue|deepskyblue|skyblue|lightskyblue|steelblue|aliceblue|dodgerblue|lightslategray|lightslategrey|slategray|slategrey|lightsteelblue|comflowerblue|royalblue|darkblue|ghostwhite|lavender|mediumblue|midnightblue|slateblue|darkslateblue|mediumslateblue|mediumpurple|rebeccapurple|blueviolet|indigo|darkorchid|darkviolet|mediumorchid|darkmagenta|plum|thistle|violet|orchid|mediumvioletred|deeppink|hotpink|lavenderblush|palevioletred|crimson|pink|lightpink";
+                    const hexCode = "(?:[0-9a-fA-F]{3}){1,2}";
+                    const textColorRegex = new RegExp(`^#(${cssColor}|${hexCode})(?:\,#(${cssColor}|${hexCode}))?( |\n)`, "g");
+                    const syntaxRegex = /^#!syntax (basic|cpp|csharp|css|erlang|go|java(?:script)?|html|json|kotlin|lisp|lua|markdown|objectivec|perl|php|powershell|python|ruby|rust|sh|sql|swift|typescript|xml)/g;
+
+                    const content = this.wikiText.substring(triple.elems[0].range.end)
+                    let match: RegExpExecArray | null;
+                    let detected = false;
+                    let detectedRange = new Range(0, 1);
+                    const doMatchIgnoring = () => {
+                        this.holderArray.forEach(v => {
+                            if (v.type !== "TripleBracketOpen" && v.range.isOverlap(detectedRange)) {
+                                v.ignore = true;
+                            }
+                        })
+                    }
+                    const doWholeIgnoring = () => {
+                        const start = this.holderArray.findIndex((v) => v.uuid === triple.elems[0].uuid);
+                        const end = this.holderArray.findIndex((v) => v.uuid === triple.elems[1].uuid);
+                        const sliced = this.holderArray
+                            .slice(start, end + 1)
+                            .toSpliced(0, 1)
+                            .toSpliced(-1, 1);
+                        sliced.forEach((v) => (v.ignore = true));
+                    }
+                    if (!detected && (match = sizingRegex.exec(content)) !== null) {
+                        detectedRange = new Range(triple.elems[0].range.end, triple.elems[0].range.end + sizingRegex.lastIndex)
+                        doMatchIgnoring();
+                        detected = true;
+                    }
+                    if (!detected && (match = wikiRegex.exec(content)) !== null) {
+                        detectedRange = new Range(triple.elems[0].range.end, elem.eolRange.end)
+                        doMatchIgnoring();
+                        detected = true;
+                    }
+                    if (!detected && (match = htmlRegex.exec(content)) !== null) {
+                        doWholeIgnoring();
+                        detected = true;
+                    }
+                    if (!detected && (match = textColorRegex.exec(content)) !== null) {
+                        detectedRange = new Range(triple.elems[0].range.end, triple.elems[0].range.end + textColorRegex.lastIndex)
+                        doMatchIgnoring();
+                        detected = true;
+                    }
+                    if (!detected && (match = syntaxRegex.exec(content)) !== null) {
+                        doWholeIgnoring();
+                        detected = true;
+                    }
+                    if (!detected) {
+                        doWholeIgnoring();
+                        detected = true;
+                    }
+                    return;
                 }
 
                 const heading = elem.group.find((v) => v.type === "Heading");
                 if (heading !== undefined) {
                     if (heading.elems.length !== 2) {
                         this.removeGroup({ group: heading });
-                        continue;
+                        flag.skipFixing = true;
+                        return;
                     }
+                    return;
                 }
 
                 const mathtag = elem.group.find((v) => v.type === "MathTag");
                 if (mathtag !== undefined) {
                     if (mathtag.elems.length !== 2) {
                         this.removeGroup({ group: mathtag });
-                        continue;
+                        flag.skipFixing = true;
+                        return;
                     }
                     const range = mathtag.elems.map((v) => v.range);
                     this.holderArray.forEach((holder) => {
@@ -679,31 +732,55 @@ export class NamuMark {
                         .toSpliced(0, 1)
                         .toSpliced(-1, 1);
                     sliced.forEach((v) => (v.ignore = true));
+                    return;
                 }
 
                 const tripleContent = elem.group.find((v) => v.type === "TripleBracketContent");
                 if (tripleContent !== undefined) {
                     tripleContent.elems.forEach((v) => (v.ignore = true));
                     this.removeGroup({ group: tripleContent });
-                    continue;
+                    flag.skipFixing = true;
+                    return;
                 }
 
                 const content = elem.group.find((v) => v.type === "Content");
                 if (content !== undefined) {
                     content.elems.forEach((v) => (v.ignore = true));
                     this.removeGroup({ group: content });
-                    continue;
+                    flag.skipFixing = true;
+                    return;
                 }
 
                 const comment = elem.group.find((v) => v.type === "Comment");
                 if (comment !== undefined) {
                     comment.elems.forEach((v) => (v.ignore = true));
                     this.removeGroup({ group: comment });
+                    flag.skipFixing = true;
+                    return;
+                }
+            }
+            for (let idx = 0; idx < this.holderArray.length; idx++) {
+                const elem = this.holderArray[idx];
+                flag.skipFixing = false;
+
+                this.holderArray = this.holderArray.filter((v) => {
+                    if (v.ignore) {
+                        v.group.forEach((group) => this.removeGroup({ group }));
+                        return false;
+                    }
+                    return true;
+                });
+
+                if (elem.fixed) {
                     continue;
                 }
 
-                for (const group of elem.group) {
-                    group.elems.forEach((v) => (v.fixed = true));
+                mainGrouping(elem, idx);
+
+                if (!flag.skipFixing) {
+                    for (const group of elem.group) {
+                        group.elems.forEach((v) => (v.fixed = true));
+                    }
                 }
             }
         };
@@ -821,7 +898,10 @@ export class NamuMark {
                     continue;
                 }
             }
+            const m = currentGrouping.name;
+            console.time(m);
             currentGrouping();
+            console.timeEnd(m);
             this.holderArray = this.holderArray.filter((v) => {
                 if (v.ignore) {
                     v.group.forEach((group) => this.removeGroup({ group }));
