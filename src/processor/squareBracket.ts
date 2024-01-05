bb7633afe6f28676d910a00ffe4d88c53f583530
jhk1090 <jhk1090.public@gmail.com>
2024-01-05 22:11:54 +0900

    squarebracket naming fix

--- src/processor/squareBracket.ts
+++ src/processor/squareBracket.ts
@@ -8,25 +8,22 @@ export const squareBracketOpenProcessor = (mark: NamuMark, props: ProcessorProps
 
     const elem = mark.holderArray[props.idx];
 
-    const adjBrackets = [elem];
+    const adjacentBrackets = [elem];
     let lastRange: Range = elem.range;
-    let bracketCount = 1; // 3 이상 부터는 모두 무쓸모
+    let bracketStack = 1; // 3 이상 부터는 모두 무쓸모
     for (const subElem of mark.holderArray.slice(props.idx + 1)) {
         if (subElem.type === "SquareBracketOpen" && lastRange.isAdjacent(subElem.range)) {
-            bracketCount++;
-            if (bracketCount > 2) {
-                // subElem.ignore = true;
-            }
-            adjBrackets.push(subElem);
+            bracketStack++;
+            adjacentBrackets.push(subElem);
             lastRange = subElem.range;
             continue;
         }
         break;
     }
 
-    squareBracketArray.push({ value: adjBrackets, max: 0 });
+    squareBracketArray.push({ value: adjacentBrackets, max: 0 });
     // 인접한 bracket은 pass해도 됨
-    props.setIdx(props.idx + adjBrackets.length - 1);
+    props.setIdx(props.idx + adjacentBrackets.length - 1);
 };
 
 export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProps) => {
@@ -34,27 +31,23 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
 
     const elem = mark.holderArray[props.idx];
 
-    const adjBrackets = [elem];
+    const adjacentBrackets = [elem];
 
     let lastRange: Range = elem.range;
-    let bracketCount = 1; // 3 이상 부터는 모두 무쓸모
+    let bracketStack = 1; // 3 이상 부터는 모두 무쓸모
     for (const subElem of mark.holderArray.slice(props.idx + 1)) {
         if (subElem.type === "SquareBracketClose" && lastRange.isAdjacent(subElem.range)) {
-            bracketCount++;
-            if (bracketCount > 2) {
-                // subElem.ignore = true;
-            }
-            adjBrackets.push(subElem);
+            bracketStack++;
+            adjacentBrackets.push(subElem);
             lastRange = subElem.range;
             continue;
         }
         break;
     }
-    const firstItem = squareBracketArray[0];
-    if (firstItem === undefined) {
-        // adjBrackets.forEach((v) => (v.ignore = true));
-        // 인접한 bracket은 pass해도 됨
-        props.setIdx(props.idx + adjBrackets.length - 1);
+    
+    const firstOpenItem = squareBracketArray[0];
+    if (firstOpenItem === undefined) {
+        props.setIdx(props.idx + adjacentBrackets.length - 1);
         return;
     }
 
@@ -62,7 +55,7 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
         const parseParenthesis = (group: Group<"SingleSquareBracket">) => {
             let parenthesisPair: [HolderElem?, HolderElem?] = [undefined, undefined];
             const startOrigin = mark.holderArray.findIndex((v) => v.uuid === bracket.value[0].uuid);
-            const endOrigin = mark.holderArray.findIndex((v) => v.uuid === adjBrackets[0].uuid);
+            const endOrigin = mark.holderArray.findIndex((v) => v.uuid === adjacentBrackets[0].uuid);
             for (const subElem of mark.holderArray.slice(startOrigin, endOrigin)) {
                 if (subElem.type === "ParenthesisOpen" && parenthesisPair[0] === undefined) {
                     parenthesisPair[0] = subElem;
@@ -81,7 +74,7 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
                 return false;
             }
             if (pEnd !== undefined) {
-                if (!pEnd.range.isAdjacent(adjBrackets[0].range)) {
+                if (!pEnd.range.isAdjacent(adjacentBrackets[0].range)) {
                     return false;
                 }
             }
@@ -89,7 +82,7 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
             const result = validMacroRegex.exec(
                 mark.wikiText.substring(
                     (bracket.value.at(-1)?.range.start as number) + 1,
-                    pEnd === undefined ? adjBrackets[0].range.start : pStart?.range.start
+                    pEnd === undefined ? adjacentBrackets[0].range.start : pStart?.range.start
                 )
             );
 
@@ -99,51 +92,55 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
             
             const elems =
                 pStart === undefined
-                    ? [adjBrackets[0], bracket.value[0]]
-                    : [adjBrackets[0], pStart as HolderElem, pEnd as HolderElem, bracket.value[0]];
+                    ? [adjacentBrackets[0], bracket.value[0]]
+                    : [adjacentBrackets[0], pStart as HolderElem, pEnd as HolderElem, bracket.value[0]];
             group.property = { name: result[0] as GroupPropertySingleSquareBracketNameType }
             mark.pushGroup({ group, elems });
             return true;
         };
 
         const prevMax = bracket.max;
-        if (adjBrackets.length > bracket.max) {
-            bracket.max = adjBrackets.length;
+        if (adjacentBrackets.length > prevMax) {
+            bracket.max = adjacentBrackets.length;
         } else {
             continue;
         }
 
         // 같은 개행 줄에 있는지 여부
-        if (bracket.value[0].eolRange.isSame(adjBrackets[0].eolRange)) {
-            const group = new Group(bracket.value.length >= 2 && bracket.max >= 2 ? "DoubleSquareBracket" : "SingleSquareBracket");
-            if (bracket.value.length >= 2 && bracket.max >= 2) {
+        if (bracket.value[0].eolRange.isSame(adjacentBrackets[0].eolRange)) {
+            const isLink = bracket.value.length >= 2 && bracket.max >= 2
+            const group = new Group(isLink ? "DoubleSquareBracket" : "SingleSquareBracket");
+            if (isLink) {
                 // 링크 문법
                 const originIndex = mark.holderArray.findIndex((v) => v.uuid === bracket.value[0].uuid);
-                const elementIndex = mark.holderArray.findIndex((v) => v.uuid === adjBrackets[0].uuid);
+                const elementIndex = mark.holderArray.findIndex((v) => v.uuid === adjacentBrackets[0].uuid);
                 const firstPipeIndex = mark.holderArray.slice(originIndex, elementIndex).findIndex((v) => v.type === "Pipe") + originIndex;
                 const elems =
                     firstPipeIndex - originIndex === -1
-                        ? [adjBrackets[0], adjBrackets[1], bracket.value[0], bracket.value[1]]
-                        : [adjBrackets[0], adjBrackets[1], mark.holderArray[firstPipeIndex], bracket.value[0], bracket.value[1]];
+                        ? [adjacentBrackets[0], adjacentBrackets[1], bracket.value[0], bracket.value[1]]
+                        : [adjacentBrackets[0], adjacentBrackets[1], mark.holderArray[firstPipeIndex], bracket.value[0], bracket.value[1]];
                 mark.pushGroup({ group, elems });
             } else {
                 // 매크로 문법
                 const isSucceed = parseParenthesis(group);
                 if (!isSucceed) {
                     bracket.max = prevMax;
-                    // adjBrackets.forEach((v) => (v.ignore = true));
                     continue;
                 }
             }
         } else {
-            if (bracket.value.length >= 2 && bracket.max >= 2) {
+            const isLink = bracket.value.length >= 2 && bracket.max >= 2
+            if (isLink) {
                 // 링크
                 const originIndex = mark.holderArray.findIndex((v) => v.uuid === bracket.value[0].uuid);
-                const elementIndex = mark.holderArray.findIndex((v) => v.uuid === adjBrackets[0].uuid);
+                const elementIndex = mark.holderArray.findIndex((v) => v.uuid === adjacentBrackets[0].uuid);
                 const firstPipeIndex = mark.holderArray.slice(originIndex, elementIndex).findIndex((v) => v.type === "Pipe") + originIndex;
+
+                /* [[
+
+                ]] 개행 중 파이프 없음 */
                 if (firstPipeIndex - originIndex === -1) {
                     bracket.max = prevMax;
-                    // adjBrackets.forEach((v) => (v.ignore = true));
                     continue;
                 }
 
@@ -171,7 +168,7 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
                 const group = new Group("DoubleSquareBracket");
                 mark.pushGroup({
                     group,
-                    elems: [adjBrackets[0], adjBrackets[1], mark.holderArray[firstPipeIndex], bracket.value[0], bracket.value[1]],
+                    elems: [adjacentBrackets[0], adjacentBrackets[1], mark.holderArray[firstPipeIndex], bracket.value[0], bracket.value[1]],
                 });
             } else {
                 // 매크로
@@ -186,11 +183,11 @@ export const squareBracketCloseProcessor = (mark: NamuMark, props: ProcessorProp
         }
 
         if ((bracket.value.length >= 2 && bracket.max >= 2) || (bracket.value.length === 1 && bracket.max >= 1)) {
-            mark.parserStore["squareBracketArray"] = squareBracketArray.filter((v) => v.value[0].range.start > adjBrackets[0].range.start);
+            mark.parserStore["squareBracketArray"] = squareBracketArray.filter((v) => v.value[0].range.start > adjacentBrackets[0].range.start);
             break;
         }
     }
 
     // 인접한 bracket은 pass해도 됨
-    props.setIdx(props.idx + adjBrackets.length - 1);
+    props.setIdx(props.idx + adjacentBrackets.length - 1);
 };
